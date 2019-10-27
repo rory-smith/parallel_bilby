979ab059a0fb3b76d5bad2f21f0196f88ea61865
Gregory Ashton <gregory.ashton@ligo.org>
2019-10-28 09:06:47 +1100

    Implement checkpointing
    This implements periodic checkpointing and reading, based on the dynesty
    implementation in bilby.

--- src/analysis.py
+++ src/analysis.py
@@ -14,6 +14,7 @@ import bilby
 import dynesty
 from dynesty.plotting import traceplot
 from dynesty import NestedSampler
+from dynesty.utils import resample_equal
 from schwimmbad import MPIPool
 from pandas import DataFrame
 
@@ -67,6 +68,144 @@ def reorder_loglikelihoods(unsorted_loglikelihoods, unsorted_samples,
     return unsorted_loglikelihoods[idxs]
 
 
+def write_checkpoint(
+        sampler, resume_file, sampling_time, search_parameter_keys):
+    """ Writes a checkpoint file
+
+    Parameters
+    ----------
+    sampler: dynesty.NestedSampler
+        The sampler object itself
+    resume_file: str
+        The name of the resume/checkpoint file to use
+    sampling_time: float
+        The total sampling time in seconds
+    search_parameter_keys: list
+        A list of the search parameter keys used in sampling (used for
+        constructing checkpoint plots and pre-results)
+    """
+    print("")
+    logger.info("Writing checkpoint file {}".format(resume_file))
+
+    current_state = dict(
+        unit_cube_samples=sampler.saved_u,
+        physical_samples=sampler.saved_v,
+        sample_likelihoods=sampler.saved_logl,
+        sample_log_volume=sampler.saved_logvol,
+        sample_log_weights=sampler.saved_logwt,
+        cumulative_log_evidence=sampler.saved_logz,
+        cumulative_log_evidence_error=sampler.saved_logzvar,
+        cumulative_information=sampler.saved_h,
+        id=sampler.saved_id,
+        it=sampler.saved_it,
+        nc=sampler.saved_nc,
+        boundidx=sampler.saved_boundidx,
+        bounditer=sampler.saved_bounditer,
+        scale=sampler.saved_scale,
+        sampling_time=sampling_time,
+    )
+
+    current_state.update(
+        ncall=sampler.ncall, live_logl=sampler.live_logl,
+        iteration=sampler.it - 1, live_u=sampler.live_u,
+        live_v=sampler.live_v, nlive=sampler.nlive,
+        live_bound=sampler.live_bound, live_it=sampler.live_it,
+        added_live=sampler.added_live
+    )
+
+    # Try to save a set of current posterior samples
+    try:
+        weights = np.exp(current_state['sample_log_weights'] -
+                         current_state['cumulative_log_evidence'][-1])
+
+        current_state['posterior'] = resample_equal(
+            np.array(current_state['physical_samples']), weights)
+        current_state['search_parameter_keys'] = search_parameter_keys
+    except ValueError:
+        logger.debug("Unable to create posterior")
+
+    with open(resume_file, 'wb') as file:
+        pickle.dump(current_state, file)
+
+    # Try to create a checkpoint traceplot
+    try:
+        fig = traceplot(sampler.results, labels=sampling_keys)[0]
+        fig.tight_layout()
+        fig.savefig(filename_trace)
+        plt.close('all')
+    except Exception:
+        pass
+
+
+def read_saved_state(resume_file, sampler):
+    """
+    Read a saved state of the sampler to disk.
+
+    The required information to reconstruct the state of the run is read from a
+    pickle file.
+
+    Parameters
+    ----------
+    resume_file: str
+        The path to the resume file to read
+    sampler: `dynesty.NestedSampler`
+        NestedSampler instance to reconstruct from the saved state.
+
+    Returns
+    -------
+    sampler: dynesty.NestedSampler
+        If a resume file exists and was succesfully read, the nested sampler
+        instance updated with the values stored to disk. If unavailable,
+        returns False
+    sampling_time: int
+        The sampling time from previous runs
+    """
+
+    if os.path.isfile(resume_file):
+        logger.info("Reading resume file {}".format(resume_file))
+        try:
+            with open(resume_file, 'rb') as file:
+                saved = pickle.load(file)
+            logger.info(
+                "Succesfuly read resume file {}".format(resume_file))
+        except EOFError as e:
+            logger.warning(
+                "Resume file reading failed with error {}".format(e))
+            return False, 0
+
+        sampler.saved_u = list(saved['unit_cube_samples'])
+        sampler.saved_v = list(saved['physical_samples'])
+        sampler.saved_logl = list(saved['sample_likelihoods'])
+        sampler.saved_logvol = list(saved['sample_log_volume'])
+        sampler.saved_logwt = list(saved['sample_log_weights'])
+        sampler.saved_logz = list(saved['cumulative_log_evidence'])
+        sampler.saved_logzvar = list(saved['cumulative_log_evidence_error'])
+        sampler.saved_id = list(saved['id'])
+        sampler.saved_it = list(saved['it'])
+        sampler.saved_nc = list(saved['nc'])
+        sampler.saved_boundidx = list(saved['boundidx'])
+        sampler.saved_bounditer = list(saved['bounditer'])
+        sampler.saved_scale = list(saved['scale'])
+        sampler.saved_h = list(saved['cumulative_information'])
+        sampler.ncall = saved['ncall']
+        sampler.live_logl = list(saved['live_logl'])
+        sampler.it = saved['iteration'] + 1
+        sampler.live_u = saved['live_u']
+        sampler.live_v = saved['live_v']
+        sampler.nlive = saved['nlive']
+        sampler.live_bound = saved['live_bound']
+        sampler.live_it = saved['live_it']
+        sampler.added_live = saved['added_live']
+        sampling_time = datetime.timedelta(
+            seconds=saved['sampling_time']).total_seconds()
+        return sampler, sampling_time
+
+    else:
+        logger.debug(
+            "No resume file {}".format(resume_file))
+        return False, 0
+
+
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["MKL_DYNAMIC"] = "0"
@@ -91,6 +230,9 @@ parser.add_argument(
     help="Dynesty sampling method (default=rwalk)")
 parser.add_argument(
     "--dynesty-walks", default=100, type=int,
+    help="Number of walks")
+parser.add_argument(
+    "--n-check-point", default=10000, type=int,
     help="Number of walks")
 
 input_args = parser.parse_args()
@@ -141,6 +283,7 @@ if likelihood.distance_marginalization:
     likelihood.parameters["luminosity_distance"] = priors["luminosity_distance"]
 
 t0 = datetime.datetime.now()
+sampling_time = 0
 with MPIPool() as pool:
     if not pool.is_master():
         pool.wait()
@@ -148,6 +291,10 @@ with MPIPool() as pool:
     POOL_SIZE = pool.size
     logger.info(f"sampling_keys={sampling_keys}")
     logger.info(priors)
+
+    filename_trace = "{}/{}_checkpoint_trace.png".format(outdir, label)
+    resume_file = "{}/{}_checkpoint_resume.pickle".format(outdir, label)
+
     sampler = NestedSampler(
         likelihood_function, prior_transform_function, len(sampling_keys),
         nlive=nlive, sample=input_args.dynesty_sample,
@@ -160,31 +307,27 @@ with MPIPool() as pool:
                       loglikelihood=True)
     )
 
+    if os.path.isfile(resume_file):
+        sampler, sampling_time = read_saved_state(resume_file, sampler)
+
     print(f"Starting sampling for job {label}, with pool size={POOL_SIZE}")
     old_ncall = sampler.ncall
-    n_check_point = 10000
     sampler_kwargs = dict(
-        print_progress=True, maxcall=n_check_point,
+        print_progress=True, maxcall=input_args.n_check_point,
         n_effective=input_args.n_effective, dlogz=input_args.dlogz)
-    filename_trace = "{}/{}_checkpoint_trace.png".format(outdir, label)
-    filename_sampler = "{}/{}_checkpoint_sampler.pkl".format(outdir, label)
+
     while True:
-        sampler_kwargs['maxcall'] += n_check_point
+        sampler_kwargs['maxcall'] += input_args.n_check_point
         sampler.run_nested(**sampler_kwargs)
         if sampler.ncall == old_ncall:
             break
         old_ncall = sampler.ncall
 
-        try:
-            fig = traceplot(sampler.results, labels=sampling_keys)[0]
-            fig.tight_layout()
-            fig.savefig(filename_trace)
-            plt.close('all')
-            with open(filename_sampler, "wb") as f:
-                pickle.dump(sampler.results, f)
-        except Exception:
-            pass
-sampling_time = (datetime.datetime.now() - t0).total_seconds()
+        sampling_time += (datetime.datetime.now() - t0).total_seconds()
+        t0 = datetime.datetime.now()
+        write_checkpoint(sampler, resume_file, sampling_time, sampling_keys)
+
+sampling_time += (datetime.datetime.now() - t0).total_seconds()
 
 out = sampler.results
 weights = np.exp(out['logwt'] - out['logz'][-1])
