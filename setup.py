b86c0561f0db2875a6de822a0cce8d289bb8e06f
Gregory Ashton <gregory.ashton@ligo.org>
2020-01-15 15:02:07 +1100

    Remove retireival from installation

--- setup.py
+++ setup.py
@@ -101,7 +101,6 @@ setup(
         "console_scripts": [
             "parallel_bilby_generation=parallel_bilby.generation:main",
             "parallel_bilby_analysis=parallel_bilby.analysis:main",
-            "parallel_bilby_retrieve_data=parallel_bilby.data_retrieval:main",
         ]
     },
     classifiers=[
