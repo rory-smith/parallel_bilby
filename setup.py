23d25fe4c5abccc581f38a57e3cc892fa1860a58
Gregory Ashton <gregory.ashton@ligo.org>
2020-01-29 23:10:56 -0600

    Improvements to the sampling method
    1) Revert scaling back to dynesty built-it
    2) Increase safety to 20
    3) Pick from the last half of nact * act rather than act
    4) Increase the nact default to 50

--- setup.py
+++ setup.py
@@ -87,7 +87,8 @@ setup(
     package_data={'parallel_bilby': [version_file]},
     install_requires=[
         "future",
-        "bilby>=0.6.1",
+        "bilby>=0.6.4",
+        "bilby_pipe>=0.3.9",
         "scipy>=1.2.0",
         "gwpy",
         "matplotlib",
