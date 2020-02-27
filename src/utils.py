0e0d368113d4e3524cca62800893f1fbea82691d
Gregory Ashton <gregory.ashton@ligo.org>
2020-02-27 16:33:38 -0600

    Parallelize initial points calculation

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+import numpy as np
+from bilby.gw import conversion
+
 
 def get_cli_args():
     """Tool to get CLI args (also makes testing easier)"""
@@ -16,3 +19,63 @@ def get_version_information():
             return f.readline().rstrip()
     except EnvironmentError:
         print("No version information file '.version' found")
+
+
+def fill_sample(args, likelihood):
+    ii, sample = args
+    sample = dict(sample).copy()
+    marg_params = likelihood.parameters.copy()
+    likelihood.parameters.update(sample)
+    sample = likelihood.generate_posterior_sample_from_marginalized_likelihood()
+    # Likelihood needs to have marg params to calculate correct SNR
+    likelihood.parameters.update(marg_params)
+    conversion.compute_snrs(sample, likelihood)
+    sample = conversion.generate_all_bbh_parameters(sample)
+    return sample
+
+
+def get_initial_point_from_prior(args):
+    (
+        prior_transform_function,
+        log_prior_function,
+        log_likelihood_function,
+        ndim,
+        calculate_likelihood,
+    ) = args
+    while True:
+        unit = np.random.rand(ndim)
+        theta = prior_transform_function(unit)
+        if bool(np.isinf(log_prior_function(theta))) is False:
+            if calculate_likelihood:
+                logl = log_likelihood_function(theta)
+                if bool(np.isinf(logl)) is False:
+                    return unit, theta, logl
+            else:
+                return unit, theta, np.nan
+
+
+def get_initial_points_from_prior(
+    ndim,
+    npoints,
+    prior_transform_function,
+    log_prior_function,
+    log_likelihood_function,
+    pool,
+    calculate_likelihood=True,
+):
+    args_list = [
+        (
+            prior_transform_function,
+            log_prior_function,
+            log_likelihood_function,
+            ndim,
+            calculate_likelihood,
+        )
+        for i in range(npoints)
+    ]
+    initial_points = pool.map(get_initial_point_from_prior, args_list)
+    u_list = [point[0] for point in initial_points]
+    v_list = [point[1] for point in initial_points]
+    l_list = [point[2] for point in initial_points]
+
+    return np.array(u_list), np.array(v_list), np.array(l_list)
