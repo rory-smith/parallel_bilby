056299a7beb5a61247268297b2943e1d6dba3ea1
Conrad Chan <conradchan@hotmail.com.au>
2021-12-03 17:10:50 +1100

    bump bilby version to 1.1.4

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     package_data={"parallel_bilby": [version_file]},
     install_requires=[
         "future",
-        "bilby>=1.1.1",
+        "bilby>=1.1.4",
         "bilby_pipe>=1.0.3",
         "scipy>=1.2.0",
         "gwpy",
